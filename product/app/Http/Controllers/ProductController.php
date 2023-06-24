6e1cbe68a7fffda3db5edf1a94f674dae135235f
Nam Hoang <hainam25101997@gmail.com>
2023-06-24 12:07:25 +0700

    save

--- product/app/Http/Controllers/ProductController.php
+++ product/app/Http/Controllers/ProductController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProductCreated;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     public function index(){
-        return 'hello';
+        $products = Product::all();
+        ProductCreated::dispatch($products->toArray())->onQueue('default');
+        return response($products, Response::HTTP_OK);
+    }
+    public function store(Request $request){
+        $products = Product::create($request->only('product_name', 'product_stock'));
+        return response($products, Response::HTTP_OK);
     }
 }
--- product/config/queue.php
+++ product/config/queue.php
@@ -71,6 +71,17 @@ return [
             'after_commit' => false,
         ],
 
+        'kafka' => [
+            'driver' => 'kafka',
+            'queue' => env('KAFKA_QUEUE', 'main_queue'),
+            'bootstrap_servers' => env('BOOTSTRAP_SERVERS'),
+            'sercurity_protocol' => env('SERCURIRTY_PROTOCOL'),
+            'sasl_mechanisms' => env('SASL_MECHANISMS'),
+            'sasl_username' => env('SASL_USERNAME'),
+            'sasl_password' => env('SASL_PASSWORD'),
+            'group_id' => env('GROUP_ID'),
+        ]
+
     ],
 
     /*
